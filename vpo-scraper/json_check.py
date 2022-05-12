43121a5f38eb96c92f6af845dceba0abeb1e73cd
Volodymyr Vasylenko <v.vasylenko92@gmail.com>
2022-05-12 14:17:52 +0300

    Add files via upload

---
+++ vpo-scraper/json_check.py
@@ -0,0 +1,20 @@
+import json
+import pathlib
+
+
+def main():
+    errJsonList = []
+    for json_f in pathlib.Path('./jsons/dovydky').glob('*\*.json'):
+        with open(json_f.absolute(), mode='r', encoding='utf-8-sig') as json_f:
+            try:
+                json_d = json.load(json_f)
+            except:
+                print(json_f.name)
+                errJsonList.append(json_f.name)
+    if len(errJsonList) != 0:
+        for jsonf in errJsonList:
+            pathlib.Path(jsonf).unlink()
+
+
+if __name__ == '__main__':
+    main()
